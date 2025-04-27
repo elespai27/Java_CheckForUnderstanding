f392cc873f216417115e46fb1fcd64b124aed825
elespai27 <elespai27@gmail.com>
2025-04-27 18:06:36 +0200

    Fix a Bug

--- src/BigDecimal/Main.java
+++ src/BigDecimal/Main.java
@@ -6,8 +6,8 @@ import java.math.RoundingMode;
 public class Main {
     public static void main(String[] args) {
 
-        BigDecimal number1 = new BigDecimal(2.6987);
-        BigDecimal number2 = new BigDecimal(3.986);
+        BigDecimal number1 = new BigDecimal("2.6987");
+        BigDecimal number2 = new BigDecimal("3.986");
 
         System.out.println(number1.add(number2).setScale(2, RoundingMode.HALF_EVEN));
         System.out.println(number1.subtract(number2));
