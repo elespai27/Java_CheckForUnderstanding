39635efa77a32b4f39ca3d64684cf8b44cd14482
elespai27 <elespai27@gmail.com>
2025-04-30 21:05:59 +0200

    Agrega archivos movidos a proyecto-main

--- src/Classes/ClasseAndObjects/Main.java
+++
@@ -1,8 +0,0 @@
-package Classes.ClasseAndObjects;
-
-public class Main {
-    public static void main(String[] args) {
-        Course course = new Course(101, 30, 10, "Introduction to Programming");
-        System.out.println(course);
-    }
-}
--- src/Classes/Inheritance/Main.java
+++
@@ -1,8 +0,0 @@
-package Classes.Inheritance;
-
-public class Main {
-    public static void main(String[] args) {
-        Invoice invoice = new Invoice("1234", "Monitor", 2, 155.80);
-        System.out.println(invoice);
-    }
-}
